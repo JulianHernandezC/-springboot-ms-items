1aa31cd676abf1016eae65c328b0b9f2134b30f2
Julián <julian.hernandez.carrion@nttdata.com>
2022-03-17 10:27:11 +0100

    ItemService->findAll

--- src/main/java/com/nttdata/app/msitems/service/impl/ItemServiceImpl.java
+++ src/main/java/com/nttdata/app/msitems/service/impl/ItemServiceImpl.java
@@ -1,12 +1,16 @@
 package com.nttdata.app.msitems.service.impl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.nttdata.app.msitems.models.Item;
+import com.nttdata.app.msitems.models.Producto;
 import com.nttdata.app.msitems.service.ItemService;
 
 @Service
@@ -17,8 +21,18 @@ public class ItemServiceImpl implements ItemService {
 
 	@Override
 	public List<Item> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Producto> productos = Arrays.asList(		
+				clienteRestProductos.getForObject("http://localhost:8001/listar", Producto[].class)
+		);
+		
+		/*List<Item> l = new ArrayList<Item>();
+		for (Producto p: productos) {
+			l.add( new Item(p, 1)   );
+		}
+		return l;*/
+		
+		return productos.stream().map( p -> new Item(p, 1) ).collect(Collectors.toList());
+		
 	}
 
 	@Override
