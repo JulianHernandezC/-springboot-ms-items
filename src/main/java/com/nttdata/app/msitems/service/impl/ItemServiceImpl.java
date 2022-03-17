2f436795b2963a4caec4c0eb3df8e6ed383f0faa
Julián <julian.hernandez.carrion@nttdata.com>
2022-03-17 10:28:58 +0100

    ItemService -> findById

--- src/main/java/com/nttdata/app/msitems/service/impl/ItemServiceImpl.java
+++ src/main/java/com/nttdata/app/msitems/service/impl/ItemServiceImpl.java
@@ -1,8 +1,9 @@
 package com.nttdata.app.msitems.service.impl;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,8 +38,12 @@ public class ItemServiceImpl implements ItemService {
 
 	@Override
 	public Item findById(Long id, Integer cantidad) {
-		// TODO Auto-generated method stub
-		return null;
+		Map<String, String> pathVariables = new HashMap<String, String>();
+		pathVariables.put("id",  id.toString());
+		
+		Producto producto = clienteRestProductos.getForObject("http://localhost:8001/listar/{id}", 
+																Producto.class, pathVariables);
+		return new Item(producto, cantidad);
 	}
 
 }
