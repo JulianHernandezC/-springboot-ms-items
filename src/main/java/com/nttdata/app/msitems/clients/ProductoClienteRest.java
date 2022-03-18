3135c6337d5e8b0a3ec0f8e4338d6cbbe09f303b
Julián <julian.hernandez.carrion@nttdata.com>
2022-03-18 09:57:00 +0100

    Incorporar Ribbon

--- src/main/java/com/nttdata/app/msitems/clients/ProductoClienteRest.java
+++ src/main/java/com/nttdata/app/msitems/clients/ProductoClienteRest.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.nttdata.app.msitems.models.Producto;
 
-@FeignClient(name="servicio-productos", url="localhost:8001")
+@FeignClient(name="servicio-productos") //, url="localhost:8001")
 public interface ProductoClienteRest {
 	
 	@GetMapping("/listar")
